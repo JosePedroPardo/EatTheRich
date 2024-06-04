18cc788ed58e0e23fda86986f8c2a5155381760d
José Pedro pardo serrano <josepedropardoserrano@gmail.com>
2024-06-04 21:35:30 +0100

    [Feature] spawn

--- Scripts/Behaviour/b_puf.gd
+++ Scripts/Behaviour/b_puf.gd
@@ -1,8 +1,11 @@
 class_name BehaviourPuf
-extends Node2D
+extends CharacterBody2D
+
+signal puf_selected
 
 @export var wait_time: int = 15
 @export var can_assemble: bool = false
+@export var selected: bool = false
 
 var myself: Puf: 
 	get: return myself
@@ -13,73 +16,56 @@ var is_baby: bool = false:
 	set(_is_baby):
 		is_baby = _is_baby
 var social_class: int = DefinitionsHelper.RANDOM_SOCIAL_CLASS
+var folow_cursor: bool = false
+var speed = 50
 
 @onready var sprite_puf: Sprite2D = $SpritePuf
 @onready var selected_circle: Sprite2D = $SelectedCircle
-@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var wait_timer: Timer = $WaitTime
-
-# Variables para el sistema de interaccion entre pufs
-@onready var interaction_area: Area2D = $InteractionArea2d
-@onready var all_pufs: Array = []
-@onready var interact_label: Label = $InteractionComponents/InteractLabel
+@onready var target = self.position
 
 func _ready():
+	animation_player.play("idle")
 	myself = Puf.new(social_class, is_baby)
 	wait_timer.wait_time = wait_time
+	set_selected(selected)
+	add_to_group("pufs", true)
 
-func _process(delta):
-	can_assemble = true if all_pufs.size() >= 3 else false
-	_update_animation_parameters()
+func _physics_process(_delta):
+	if folow_cursor:
+		if selected:
+			target = get_global_mouse_position()
+			animation_player.play("Walk")
+		self.velocity = position.direction_to(target) * speed
+	if position.distance_to(target) > 10:
+		self.move_and_slide()
+	else:
+		animation_player.stop()
 
 func _input(event):
 	if event.is_action_pressed("assemble") and can_assemble:
 		print("se juntan")
+	if event.is_action_pressed("right_click"):
+		folow_cursor = true
+	if event.is_action_released("right_click"):
+		folow_cursor = false
+
+func set_selected(_selected: bool):
+	selected = _selected
+	selected_circle.visible = _selected
 
 func get_social_class():
-	match(myself.social_class):
+	match(myself.social_class):	
 		0: return DefinitionsHelper.POOR
 		1: return DefinitionsHelper.RICH
 		_: return "null"
 
-func _update_animation_parameters():
-	pass # TODO: Hacer el cambio de animación cuando esté el sistema de movimiento
-
-# Métodos de interacción
-func _on_interact_area_area_shape_entered(area):
-	print(area)
-	print(all_pufs)
-	all_pufs.push_front(area)
-	update_interactions()
-
-func _on_interact_area_area_shape_exited(area):
-	print(area)
-	all_pufs.erase(area)
-	update_interactions()
-
-func update_interactions():
-	if all_pufs:
-		interact_label.text = all_pufs[0].interact_label
-	else: interact_label.text = ""
-
-func execute_interactions():
-	if all_pufs:
-		var cur_interaction = all_pufs[0]
-		match cur_interaction.interact_type:
-			InteractHelper.INTERACTION_PRINT_TEXT: print(cur_interaction)
-
-func _on_rigid_body_2d_body_entered(body):
-	print(body)
-	all_pufs.push_front(body)
-	update_interactions()
-
-func _on_rigid_body_2d_body_exited(body):
-	print(body)
-	all_pufs.erase(body)
-	update_interactions()
-
-func _on_character_body_2d_input_event(viewport, event, shape_idx):
+func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			selected_circle.visible = true
+			selected_circle.visible = !selected_circle.visible
+			emit_signal("puf_selected", self)
 
+func get_json_serialize():
+	return myself.jsonSerialize()
--- Scripts/Managers/manager_pufs.gd
+++ Scripts/Managers/manager_pufs.gd
@@ -1,18 +1,43 @@
 class_name ManagerPufs
 extends Node2D
 
-@export var total_pufs: Array = []
+@export var limit_spwan_puf: int = 15
+@export var spawn_time: int = 15
+@export var spawn_initial_pufs: Array = []
+
+# Variables para el spawn
+@export var spawn_position: Vector2 = Vector2.ZERO
+@export var min_width: float = 100.0
+@export var max_width: float = 200.0
+@export var min_height: float = 100.0 
+@export var max_height: float = 200.0
 
 # Variables para el sistema de selección de pufs
+@onready var parent: Node2D = get_node("../")
 @onready var selected_pufs: Array = []
+@onready var puf: PackedScene = preload("res://Scenes/puf.tscn")
+@onready var timer_spawn: Timer = $Timer_spawn
+@onready var mouse_position
 
-func _save_selected(selected: Node2D):
-	selected_pufs.push_back(selected)
+func _ready():
+	timer_spawn.wait_time = spawn_time
+	timer_spawn.start()
 
-func _is_in_array_selected(selected: Node2D) -> bool:
-	return selected_pufs.has(selected)
+func _process(delta):
+	if spawn_initial_pufs.size() == limit_spwan_puf:
+		timer_spawn.stop()
 
-func _remove_selected(selected: Node2D):
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouse_position = get_viewport().get_mouse_position()
+
+func _save_selected(selected: Node2D, array: Array):
+	array.push_back(selected)
+
+func _is_in_array_selected(selected: Node2D, array: Array) -> bool:
+	return array.has(selected)
+
+func _remove_selected(selected: Node2D, array: Array):
 	selected_pufs.erase(selected)
 
 func _deselect_all():
@@ -26,3 +51,40 @@ func _call_to_method(selected: Node3D, method: String):
 func _clean_selecteds():
 	_deselect_all()
 	selected_pufs.clear()
+
+func _on_timer_spawn_timeout():
+	var new_puf = puf.instantiate()
+	new_puf.position = get_random_position()
+	parent.add_child(new_puf)
+	print(new_puf.get_json_serialize())
+	_save_selected(new_puf, spawn_initial_pufs)
+
+func get_random_position():
+	var vpr_width = randf_range(min_width, max_width)
+	var vpr_height = randf_range(min_height, max_height)
+	var top_left = Vector2(spawn_position.x - vpr_width/2, spawn_position.y - vpr_height/2)
+	var top_right = Vector2(spawn_position.x + vpr_width/2, spawn_position.y - vpr_height/2)
+	var bottom_left = Vector2(spawn_position.x - vpr_width/2, spawn_position.y + vpr_height/2)
+	var bottom_right = Vector2(spawn_position.x + vpr_width/2, spawn_position.y + vpr_height/2)
+	var pos_side = ["up", "down", "right", "left"].pick_random()
+	var spawn_pos1 = Vector2.ZERO
+	var spawn_pos2 = Vector2.ZERO
+	
+	match pos_side:
+		"up":
+			spawn_pos1 = top_left
+			spawn_pos2 = top_right
+		"down":
+			spawn_pos1 = bottom_left
+			spawn_pos2 = bottom_right
+		"right":
+			spawn_pos1 = top_right
+			spawn_pos2 = bottom_right
+		"left":
+			spawn_pos1 = top_left
+			spawn_pos2 = bottom_left
+	
+	var x_spawn = randf_range(spawn_pos1.x, spawn_pos2.x)
+	var y_spawn = randf_range(spawn_pos1.y, spawn_pos2.y)
+	return Vector2(x_spawn, y_spawn)
+
